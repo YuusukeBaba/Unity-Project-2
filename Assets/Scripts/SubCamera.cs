0a3057cf89ca1d3804322c8f1d1521edaba39d75
Yuusuke Baba <yuusuke052@gmail.com>
2016-01-24 16:28:42 +0900

    プロジェクトの作成

---
+++ Assets/Scripts/SubCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(GameObject.Find("MainCamera"))
+			Destroy(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
